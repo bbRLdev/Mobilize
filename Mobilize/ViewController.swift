7dc0b7d246f1de807f2b7300a2a590eae8f1e184
Michael Labarca <michael.labarca@utexas.edu>
2020-10-13 17:51:16 -0500

    Initial Commit

---
+++ Mobilize/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mobilize
+//
+//  Created by Michael Labarca on 10/13/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
