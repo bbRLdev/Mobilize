11fb7470d6ef0319774b0ea6ccb7fb9d88a79054
Michael Labarca <michael.labarca@utexas.edu>
2020-11-17 17:24:02 -0600

    Fixed AddMediaVC issues

--- Mobilize/Controllers/CreateEvent/AddMediaViewController.swift
+++ Mobilize/Controllers/CreateEvent/AddMediaViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import FirebaseStorage
+import FirebaseUI
 import FirebaseAuth
 
 class AddMediaViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
@@ -14,9 +15,10 @@ class AddMediaViewController: UIViewController, UIImagePickerControllerDelegate,
     var event: EventModel!
     var eventSoFar: [String : Any] = [:]
     var imagePicker = UIImagePickerController()
-    var images: [UIImage] = []
+    var images: [UIImage?] = []
     var cells: [ImageCell] = []
     let segueId = "QASegueId"
+    var imageRefs: [String] = []
     
     let storage = Storage.storage()
     let auth = Auth.auth()
@@ -24,19 +26,90 @@ class AddMediaViewController: UIViewController, UIImagePickerControllerDelegate,
     
     @IBOutlet weak var eventPicturesCollection: UICollectionView!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        imagePicker.delegate = self
-        eventPicturesCollection.dataSource = self
-        eventPicturesCollection.delegate = self
         if event != nil {
             // we must be editing an already created event, so
             // load the images
             images = retrieveImages()
         }
-        
+        setUpCells()
+        imagePicker.delegate = self
+        eventPicturesCollection.dataSource = self
+        eventPicturesCollection.delegate = self
+
+        eventPicturesCollection.reloadData()
 
         // Do any additional setup after loading the view.
+    }
+    
+    func setUpCells() {
+        var placeholderCount: Int = 0
+        let storageRef = Storage.storage().reference(forURL: "gs://mobilize-77a05.appspot.com")
+        
+        var numImages = 0
+        if images.count != 0 {
+            numImages = images.count
+            for i in 0...numImages - 1 {
+                let imageCell = createImageCell()
+                imageCell.hasImage = true
+                let placeholderImage = UIImage(systemName: "questionmark")
+                if images[i] != nil && images[i]!.isEqual(placeholderImage) {
+                    let ref = storageRef.child("events/\(event.eventID!)").child(imageRefs[placeholderCount])
+                    imageCell.imageMeta.sd_setImage(with: ref, placeholderImage: images[i])
+                    imageCell.imageIndex = i
+                    imageCell.refIndex = placeholderCount
+                    placeholderCount += 1
+                } else if images[i] != nil {
+                    imageCell.imageMeta.image = images[i]
+                    imageCell.imageIndex = i
+                }
+                imageCell.editButtonMeta.tag = i
+                let smallConfig = UIImage.SymbolConfiguration(pointSize: 24, weight: .medium, scale: .small)
+                let deleteImage = UIImage(systemName: "xmark", withConfiguration: smallConfig)?
+                           .withTintColor(.white, renderingMode: .alwaysOriginal)
+                imageCell.editButtonMeta.setImage(deleteImage, for: .normal)
+                cells.append(imageCell)
+            }
+        }
+        for i in numImages...7 {
+            let imageCell = createImageCell()
+            imageCell.editButtonMeta.tag = i
+            imageCell.hasImage = false
+            let smallConfig = UIImage.SymbolConfiguration(pointSize: 24, weight: .medium, scale: .small)
+            let plusImage = UIImage(systemName: "plus", withConfiguration: smallConfig)?
+                       .withTintColor(.white, renderingMode: .alwaysOriginal)
+            imageCell.editButtonMeta.setImage(plusImage, for: .normal)
+            cells.append(imageCell)
+        }
+    }
+    
+    // Create a basic ImageCell by setting up the looks that are shared between all regardless of whether there
+    // is an image yet.
+    func createImageCell() -> ImageCell {
+        let imageCell: ImageCell = ImageCell()
+        let cellImage: UIImageView = UIImageView()
+        let insets = UIEdgeInsets(top: -16, left: -16, bottom: -16, right: -16)
+        cellImage.image = cellImage.image?.withAlignmentRectInsets(insets)
+        cellImage.layer.cornerRadius = 8.0
+        cellImage.layer.borderWidth = 2.0
+        cellImage.clipsToBounds = true
+        cellImage.layer.borderColor = UIColor.lightGray.cgColor
+        imageCell.imageMeta = cellImage
+        
+        let cellButton: UIButton = UIButton()
+        cellButton.layer.zPosition = 99
+        let largeConfig = UIImage.SymbolConfiguration(pointSize: 32, weight: .regular, scale: .large)
+        let circleImage = UIImage(systemName: "circle.fill", withConfiguration: largeConfig)?
+            .withTintColor(.red, renderingMode: .alwaysOriginal)
+        cellButton.setBackgroundImage(circleImage, for: .normal)
+        imageCell.editButtonMeta = cellButton
+        imageCell.editButtonMeta.addTarget(self, action: #selector(onAddPicturePressed(_:)), for: .touchUpInside)
+
+
+        
+        return imageCell
     }
     
     @IBAction func onAddPicturePressed(_ sender: UIButton) {
@@ -49,8 +122,15 @@ class AddMediaViewController: UIViewController, UIImagePickerControllerDelegate,
             let controller = UIAlertController(title: "Delete Photo?", message: "", preferredStyle: .actionSheet)
             controller.addAction(UIAlertAction(title:"Delete", style: .destructive, handler: {
                 _ in
-                self.images.remove(at: sender.tag)
+                let cell = self.cells[sender.tag]
+                if let refIndex = cell.refIndex {
+                    self.imageRefs.remove(at: refIndex)
+                    self.images.remove(at: cell.imageIndex)
+                } else {
+                    self.images.remove(at: cell.imageIndex)
+                }
                 self.cells = []
+                self.setUpCells()
                 self.eventPicturesCollection.reloadData()
             }))
             controller.addAction(UIAlertAction(title:"Cancel", style: .cancel))
@@ -62,7 +142,8 @@ class AddMediaViewController: UIViewController, UIImagePickerControllerDelegate,
         if let image = info[.editedImage] as? UIImage {
             self.images.append(image)
         }
-        cells = []
+        self.cells = []
+        setUpCells()
         eventPicturesCollection.reloadData()
         dismiss(animated: true, completion: nil)
     }
@@ -77,30 +158,29 @@ class AddMediaViewController: UIViewController, UIImagePickerControllerDelegate,
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == segueId,
-           let nextVC = segue.destination as? QAandConfirmVC{
-            nextVC.images = images
+           let nextVC = segue.destination as? QAandConfirmVC {
+            event.photoURLCollection = imageRefs
+            eventSoFar["photoIDCollection"] = nil
+            var imageRefList = [[String : String]]()
+            var count: Int = 0
+            for ref in imageRefs {
+                imageRefList.append(["\(count)" : ref])
+                count += 1
+            }
+            eventSoFar["photoIDCollection"] = imageRefList
             nextVC.event = event
             nextVC.eventSoFar = eventSoFar
         }
     }
     
     // Download the images from our existing model.
-    func retrieveImages() -> [UIImage] {
-        var returnVal: [UIImage] = []
-        if let uid = event.organizerUID {
-            if uid == auth.currentUser?.uid {
-                for url in event.photoURLCollection {
-                    let downloadURL = URL(string: url)
-                    let data = try? Data(contentsOf: downloadURL!)
-                    if let imageData = data {
-                        let image = UIImage(data: imageData)
-                        returnVal.append(image!)
-                    }
-                }
-
-            }
+    func retrieveImages() -> [UIImage?] {
+        var ret: [UIImage?] = []
+        for i in 0...event.photoURLCollection.count - 1 {
+            imageRefs.append(event.photoURLCollection[i])
+            ret.append(UIImage(systemName: "questionmark"))
         }
-        return returnVal
+        return ret
     }
     
 
@@ -110,49 +190,25 @@ class AddMediaViewController: UIViewController, UIImagePickerControllerDelegate,
 
 extension AddMediaViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 8
+        return cells.count
     }
 }
 extension AddMediaViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = eventPicturesCollection.dequeueReusableCell(withReuseIdentifier: "MediaCellId", for: indexPath) as! ImageCell
-        cell.image.image = nil
-        cell.hasImage = false
-                // reference to my variable "image" in MyImageCell.swift
-        if(indexPath.row < images.count) {
-            cell.image.image = images[indexPath.row]
-            cell.hasImage = true
-        }
-        let insets = UIEdgeInsets(top: -16, left: -16, bottom: -16, right: -16)
-        cell.image.image = cell.image.image?.withAlignmentRectInsets(insets)
-        cell.image.layer.cornerRadius = 8.0
-        cell.image.layer.borderWidth = 2.0
-        cell.image.clipsToBounds = true
-        cell.image.layer.borderColor = UIColor.lightGray.cgColor
+        let metaCell = cells[indexPath.row]
+        cell.image.image = metaCell.imageMeta.image
+        cell.image.layer.cornerRadius = metaCell.imageMeta.layer.cornerRadius
+        cell.image.layer.borderWidth = metaCell.imageMeta.layer.borderWidth
+        cell.image.clipsToBounds = metaCell.imageMeta.clipsToBounds
+        cell.image.layer.borderColor = metaCell.imageMeta.layer.borderColor
         
-
-        cell.editButton.layer.zPosition = 99
-//        cell.editButton.trailingAnchor.constraint(equalTo: cell.trailingAnchor, constant: 4).isActive = true
-//        cell.editButton.bottomAnchor.constraint(equalTo: cell.bottomAnchor, constant: 4).isActive = true
-
-        
-        let largeConfig = UIImage.SymbolConfiguration(pointSize: 32, weight: .regular, scale: .large)
-        let smallConfig = UIImage.SymbolConfiguration(pointSize: 24, weight: .medium, scale: .small)
-
-        let plusCircleImage = UIImage(systemName: "circle.fill", withConfiguration: largeConfig)?
-            .withTintColor(.red, renderingMode: .alwaysOriginal)
-        let plusImage = UIImage(systemName: "plus", withConfiguration: smallConfig)?
-            .withTintColor(.white, renderingMode: .alwaysOriginal)
-        let deleteImage = UIImage(systemName: "xmark", withConfiguration: smallConfig)?
-            .withTintColor(.white, renderingMode: .alwaysOriginal)
-        cell.editButton.setBackgroundImage(plusCircleImage, for: .normal)
-        cell.editButton.setImage(plusImage, for: .normal)
-        if(indexPath.row < images.count) {
-            cell.editButton.setImage(deleteImage, for: .normal)
-        }
+        cell.editButton.layer.zPosition = metaCell.editButtonMeta.layer.zPosition
+        cell.editButton.setBackgroundImage(metaCell.editButtonMeta.backgroundImage(for: .normal), for: .normal)
+        cell.editButton.setImage(metaCell.editButtonMeta.image(for: .normal), for: .normal)
         cell.editButton.addTarget(self, action: #selector(onAddPicturePressed(_:)), for: .touchUpInside)
         cell.editButton.tag = indexPath.row
-        cells.append(cell)
+        
         return cell
     }
 }
@@ -195,4 +251,8 @@ class ImageCell: UICollectionViewCell {
     @IBOutlet weak var image: UIImageView!
     @IBOutlet weak var editButton: UIButton!
     var hasImage: Bool = false
+    var imageIndex: Int = 0
+    var refIndex: Int?
+    var imageMeta: UIImageView!
+    var editButtonMeta: UIButton!
 }
